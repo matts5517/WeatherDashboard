0acde8c11c07e04138613e8ab881d3cc1ef73334
Matt Silveira <Matts_Home@Matthews-MacBook-Air.local>
2017-05-06 15:36:20 -0600

    First push to repo

---
+++ ExtremeWeatherSite_v4/js/currentObs.js
@@ -0,0 +1,162 @@
+
+
+var test = function(){
+	console.log('print test')
+};
+
+var currentObsLayers = function(val){
+	try{
+		map.removeSource('wind');
+        map.removeLayer('wind');
+        map.removeSource('temp_f');
+        map.removeLayer('temp_f');
+        map.removeSource('windGust');
+        map.removeLayer('windGust');
+        map.removeSource('humidity');
+        map.removeLayer('humidity');
+        // map.removeSource('pressure');
+        // map.removeLayer('pressure');
+    }
+    catch(err){
+        ''
+    }
+	console.log(val);
+	var stopList; // set stopList var
+	// determine which color ramp to use //////////////
+	if(val == 'wind'){
+		stopList = windList;
+	} else if (val == 'temp_f'){
+		stopList = tempList
+	}else if (val == 'windGust'){
+		stopList = windGustList
+	}else if (val == 'humidity'){
+		stopList = humidityList
+	}else if(val == 'pressure'){
+		stopList = pressureList
+	}
+	console.log(val)
+	map.addSource(val, {
+        "type": "geojson",
+        "data": jsonData
+    });
+    map.addLayer({
+        "id": val,
+        "source": val,
+        "type": "circle",
+        'paint': {
+            'circle-color': {
+                property: val,
+                stops: stopList
+            }
+        },
+    });
+};
+
+var currentSliderChange = function(id, lowerVal, upperVal){
+	try{
+		map.removeSource('wind');
+        map.removeLayer('wind');
+        map.removeSource('temp_f');
+        map.removeLayer('temp_f');
+        map.removeSource('windGust');
+        map.removeLayer('windGust');
+        map.removeSource('humidity');
+        map.removeLayer('humidity');
+        // map.removeSource('pressure');
+        // map.removeLayer('pressure');
+    }
+    catch(err){
+        ''
+    }
+	if(id == 'tempSldr'){
+		var val = 'temp_f'
+		var stop = tempList;
+		// called at end of slide. use change to ask server for data
+	    var features = $.grep(jsonData.features, function(element, index){
+	          return element.properties.temp_f >= lowerVal && element.properties.temp_f <= upperVal;
+	    });
+	}else if (id == 'windSldr'){
+		var val = 'wind'
+		var stop = windList;
+		// called at end of slide. use change to ask server for data
+	    var features = $.grep(jsonData.features, function(element, index){
+	          return element.properties.wind >= lowerVal && element.properties.wind <= upperVal;
+	    });
+	}else if (id == 'windGustSldr'){
+		var val = 'windGust'
+		var stop = windGustList;
+		// called at end of slide. use change to ask server for data
+	    var features = $.grep(jsonData.features, function(element, index){
+	          return element.properties.windGust >= lowerVal && element.properties.windGust <= upperVal;
+	    });
+	}else if (id == 'humiditySldr'){
+		var val = 'humidity'
+		var stop = humidityList;
+		// called at end of slide. use change to ask server for data
+	    var features = $.grep(jsonData.features, function(element, index){
+	          return element.properties.humidity >= lowerVal && element.properties.humidity <= upperVal;
+	    });
+	}
+
+	try{
+        map.removeSource(val);
+        map.removeLayer(val);
+    }
+    catch(err){
+        ''
+    }
+
+	var features = {"type":"FeatureCollection", features } 
+	console.log(features);
+
+	map.addSource(val, {
+        "type": "geojson",
+        "data": features
+    });
+
+    map.addLayer({
+        "id": val,
+        "source": val,
+        "type": "circle",
+        'paint': {
+            'circle-color': {
+                property: val,
+                stops: stop
+            }
+        },
+    });
+
+};
+
+
+// // Build the functionality for the popup ////////////////////////
+//   map.on('mousemove', function (e) {
+//         var features = map.queryRenderedFeatures(e.point, { layers: [val] });
+//         // Change the cursor style as a UI indicator.
+//         map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+//         if (!features.length) {
+//             popup.remove();
+//             return;
+//         }
+//         var feature = features[0];
+//         // Populate the popup and set its coordinates
+//         // based on the feature found.
+//         popup.setLngLat(feature.geometry.coordinates)
+//             .setHTML(feature.properties.temp_f + '&deg' + '<br>' + feature.properties.wind + 'MPH')
+//             .addTo(map);
+//     })
+
+// 	// Use the same approach as above to indicate that the symbols are clickable
+// 	// by changing the cursor style to 'pointer'.
+// 	map.on('mousemove', function (e) {
+// 	    var features = map.queryRenderedFeatures(e.point, { layers: [val] });
+// 	    map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+// 	});
+// 	// Create a popup, but don't add it to the map yet.
+// 	var popup = new mapboxgl.Popup({
+// 	    closeButton: true,
+// 	    closeOnClick: false
+// 	});
+// });
+
+
---
+++ ExtremeWeatherSite_v4/python/severeStormReport_Extract.py
@@ -0,0 +1,68 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      msilveira
+#
+# Created:     28/02/2017
+# Copyright:   (c) msilveira 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import math, os, sys, time, datetime, xml.etree.ElementTree, json
+from email.Utils import formatdate
+print  time.asctime()
+start = time.time()
+#Script starts here
+#-------------------------------------------------------------------------------
+import urllib
+import csv
+from datetime import datetime
+utcTime = datetime.utcnow()
+print utcTime
+
+url = 'http://www.spc.noaa.gov/climo/reports/today_raw.csv'
+urllib.urlretrieve (url,'fileSevere.csv')
+
+jsonData = []
+with open("fileSevere.csv", "rb") as f:
+    reader = csv.reader(f, delimiter="\t")
+    for i, line in enumerate(reader):
+        line = str(line)[2:-2]
+        listLine = line.split(',')
+        time = listLine[0]
+        if 'Raw Tornado LSR' in str(listLine):
+            reportType = 'tornado'
+        if 'Raw Wind/Gust LSR' in str(listLine):
+            reportType = 'wind'
+        if 'Raw Hail LSR' in str(listLine):
+            reportType = 'hail'
+        try:
+            if reportType == 'hail':
+                eventType = '3'
+            if reportType == 'wind':
+                eventType = '2'
+            if reportType == 'tornado':
+                eventType = '1'
+
+            timeReported = listLine[0]
+            size = listLine[1]
+            location = listLine[2]
+            county = listLine[3]
+            state = listLine[4]
+            lat = listLine[5]
+            lon  = listLine[6]
+            comments = listLine[7]
+
+            d1 = {'type':'Feature','geometry':{'type':'Point', 'coordinates': [float(lon), float(lat)]},
+            'properties': {'timeReported': timeReported,'size': size, 'location':location,'county':county ,'state':state,'eventType':eventType,'comments':comments , 'marker-symbol':'default_marker'}}
+            jsonData.append(d1)
+
+        except:
+            print 'no index for this var'
+
+jsonData = {'type': 'FeatureCollection', 'features': jsonData}
+print jsonData
+with open('severeData.json', 'w') as outfile:
+    json.dump(jsonData, outfile, indent=4, separators=(',', ':'))
+
