a5fc2fc1bc0a75dda5fd0bac0d0a87d3937a2089
Matt Silveira <Matts_Home@Matthews-MacBook-Air.local>
2018-03-14 20:41:25 -0600

    after major style overhaul

--- js/ui.js
+++ js/ui.js
@@ -1,65 +1,99 @@
 // js file for handling the UI of the site ////////////////////////////////////////////////////////////////////////////////////////
 
+// on main cb click
+	$('.mainCB input').on('click', function(e){
+		console.log(e)
+		if(e.currentTarget.checked){
+			$(e.currentTarget).parent().parent().next().slideDown()
+			console.log('is check')
+			console.log($(e.currentTarget).parent().parent().next().slideDown())
+			// loop through inputs below and figure out which are checked
+			console.log($(e.currentTarget).parent().parent().next().find('input'))
+				// based on if checked 
+
+				// turn on those layer groups by calling the appropriate function
+
+
+		}else{
+			console.log('not checked');
+			$(e.currentTarget).parent().parent().next().slideUp()
+		}
+	})
+	// if checked
+
+	// slide down control wrapper below
+
+	// figure out which radio/cb button is checked and trigger click
+
+
+
+
+
+
+
+
+
+
 
 // Build out the animation for the attribute window expand collapse functionality ////////////////////////////////////////////////
-$("#attributeMinBtn").click(function () {
-	var width = $("#sideBarAttWrapper").width();
-	if(width > 250){
-		$("#sideBarAttWrapper").animate({ width: '160px'});
-		$('#attributeMinBtn').css({ "transform": "rotate(0deg)"})
-		$('#currentObs-CntrlWrap').find('.cb_slideContent').each(function(i,v){
-          $(v).slideUp();
-    	})
+// $("#attributeMinBtn").click(function () {
+// 	var width = $("#sideBarAttWrapper").width();
+// 	if(width > 250){
+// 		$("#sideBarAttWrapper").animate({ width: '160px'});
+// 		$('#attributeMinBtn').css({ "transform": "rotate(0deg)"})
+// 		$('#currentObs-CntrlWrap').find('.cb_slideContent').each(function(i,v){
+//           $(v).slideUp();
+//     	})
 
-	}else{
-		$("#sideBarAttWrapper").animate({ width: '300px'});
-		$('#attributeMinBtn').css({ "transform": "rotate(180deg)"})
-		$('#currentObs-CntrlWrap').find('input').each(function(i,v){
-			if(v.checked){
-				$(v).parent().next().children().slideDown();
-			}
-		})
-	}
-});
+// 	}else{
+// 		$("#sideBarAttWrapper").animate({ width: '300px'});
+// 		$('#attributeMinBtn').css({ "transform": "rotate(180deg)"})
+// 		$('#currentObs-CntrlWrap').find('input').each(function(i,v){
+// 			if(v.checked){
+// 				$(v).parent().next().children().slideDown();
+// 			}
+// 		})
+// 	}
+// });
 
 
-$( ".pillCheckbox" ).on( "click", function(c) {
-	// turn off all the current obs layers now when clicking a different pill box
-	if(c.currentTarget.id != 'currentObs-Pill'){
-		$(app.lyrList).each(function(v,c){
-	        map.setLayoutProperty(c, 'visibility', 'none');
-	    })
-	}else{ // if the current obs pill is clicked tun on the appropriate layer as indicated by the radio button
-		// loop through rad butons to find which one is checked
-		$($('#currentObs-CntrlWrap').find('input')).each(function(v,c){
-			if(c.checked){
-				map.setLayoutProperty(c.value, 'visibility', 'visible');
-			}
-		})
-	}
+// $( ".pillCheckbox" ).on( "click", function(c) {
+// 	// turn off all the current obs layers now when clicking a different pill box
+// 	if(c.currentTarget.id != 'currentObs-Pill'){
+// 		$(app.lyrList).each(function(v,c){
+// 	        map.setLayoutProperty(c, 'visibility', 'none');
+// 	    })
+// 	}else{ // if the current obs pill is clicked tun on the appropriate layer as indicated by the radio button
+// 		// loop through rad butons to find which one is checked
+// 		$($('#currentObs-CntrlWrap').find('input')).each(function(v,c){
+// 			if(c.checked){
+// 				map.setLayoutProperty(c.value, 'visibility', 'visible');
+// 			}
+// 		})
+// 	}
 
-	var pillID = c.currentTarget.id.split('-')[0];
-	var newID;
-	// add orange css color text to show what is selected
-	let test = $('.sideBarNavigation').find('.pillCheckbox')
-	console.log('look 2	')
-	$('.sideBarNavigation').find('.pillCheckbox').each(function(i,v){
-          $(v).css('color', 'white')
-    })
-	$(c.currentTarget).css('color', 'orange')
-	// slide down the appropriate control wrapper section.
-	$( ".cntrlWrap" ).each(function(v,c){
-		var ctrID = c.id.split('-')[0];
-		var ctrID2 = c.id;
-		if(ctrID == pillID){
-			newID = pillID + '-CntrlWrap';
-			$( "#" + newID ).slideDown();
-		}else{
-			$( "#" + ctrID2 ).slideUp();
-		}
-	});
+// 	var pillID = c.currentTarget.id.split('-')[0];
+// 	var newID;
+// 	// add orange css color text to show what is selected
+// 	let test = $('.sideBarNavigation').find('.pillCheckbox')
+// 	console.log('look 2	')
+// 	$('.sideBarNavigation').find('.pillCheckbox').each(function(i,v){
+//           $(v).css('color', 'white')
+//     })
+// 	$(c.currentTarget).css('color', 'orange')
+// 	// slide down the appropriate control wrapper section.
+// 	$( ".cntrlWrap" ).each(function(v,c){
+// 		var ctrID = c.id.split('-')[0];
+// 		var ctrID2 = c.id;
+// 		if(ctrID == pillID){
+// 			newID = pillID + '-CntrlWrap';
+// 			$( "#" + newID ).slideDown();
+// 		}else{
+// 			$( "#" + ctrID2 ).slideUp();
+// 		}
+// 	});
 	
-});
+// });
 
 // Handle radio buttons for current observations //////////////////////////////////////////////////////////////////
 $( "#currentObs-CntrlWrap input" ).on( "click", function(c) {
@@ -92,61 +126,61 @@ $( "#severe-CntrlWrap input" ).on( "click", function(c) {
 
 //layout range slider
 // Temp slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
-$('#tempSldr').slider({range:true, min:-20, max:130, values:[0,100]});
-$('#tempSldr').on('slidechange', function( event, ui ) {
-	var id = event.target.id;
-	var lowerVal = ui.values[0]
-    var upperVal  = ui.values[1]
-	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
-});
-$('#tempSldr').on('slide', function( event, ui ) {
-	$('#tempSldr').prev().find('span').each(function(i,v){
-        $(v).html(ui.values[i])
-        test();
-    })
-});
+// $('#tempSldr').slider({range:true, min:-20, max:130, values:[0,100]});
+// $('#tempSldr').on('slidechange', function( event, ui ) {
+// 	var id = event.target.id;
+// 	var lowerVal = ui.values[0]
+//     var upperVal  = ui.values[1]
+// 	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
+// });
+// $('#tempSldr').on('slide', function( event, ui ) {
+// 	$('#tempSldr').prev().find('span').each(function(i,v){
+//         $(v).html(ui.values[i])
+//         test();
+//     })
+// });
 
-// Wind slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
-$('#windSldr').slider({range:true, min:0, max:100, values:[0,100]});
-$('#windSldr').on('slidechange', function( event, ui ) {
-	var id = event.target.id;
-	var lowerVal = ui.values[0]
-    var upperVal  = ui.values[1]
-	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
-});
-$('#windSldr').on('slide', function( event, ui ) {
-	$('#windSldr').prev().find('span').each(function(i,v){
-        $(v).html(ui.values[i])
-    })
-});
-// Wind gust slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
-$('#windGustSldr').slider({range:true, min:0, max:100, values:[0,100]});
-$('#windGustSldr').on('slidechange', function( event, ui ) {
-	var id = event.target.id;
-	var lowerVal = ui.values[0]
-    var upperVal  = ui.values[1]
-	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
-});
-$('#windGustSldr').on('slide', function( event, ui ) {
-	console.log('wind gust slider slide');
-	$('#windGustSldr').prev().find('span').each(function(i,v){
-        $(v).html(ui.values[i])
-    })
-});
-// Humidity slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
-$('#humiditySldr').slider({range:true, min:0, max:100, values:[0,100]});
-$('#humiditySldr').on('slidechange', function( event, ui ) {
-	var id = event.target.id;
-	var lowerVal = ui.values[0]
-    var upperVal  = ui.values[1]
-	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
-});
-$('#humiditySldr').on('slide', function( event, ui ) {
-	console.log('humidity slider slide');
-	$('#humiditySldr').prev().find('span').each(function(i,v){
-        $(v).html(ui.values[i])
-    })
-});
+// // Wind slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
+// $('#windSldr').slider({range:true, min:0, max:100, values:[0,100]});
+// $('#windSldr').on('slidechange', function( event, ui ) {
+// 	var id = event.target.id;
+// 	var lowerVal = ui.values[0]
+//     var upperVal  = ui.values[1]
+// 	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
+// });
+// $('#windSldr').on('slide', function( event, ui ) {
+// 	$('#windSldr').prev().find('span').each(function(i,v){
+//         $(v).html(ui.values[i])
+//     })
+// });
+// // Wind gust slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
+// $('#windGustSldr').slider({range:true, min:0, max:100, values:[0,100]});
+// $('#windGustSldr').on('slidechange', function( event, ui ) {
+// 	var id = event.target.id;
+// 	var lowerVal = ui.values[0]
+//     var upperVal  = ui.values[1]
+// 	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
+// });
+// $('#windGustSldr').on('slide', function( event, ui ) {
+// 	console.log('wind gust slider slide');
+// 	$('#windGustSldr').prev().find('span').each(function(i,v){
+//         $(v).html(ui.values[i])
+//     })
+// });
+// // Humidity slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
+// $('#humiditySldr').slider({range:true, min:0, max:100, values:[0,100]});
+// $('#humiditySldr').on('slidechange', function( event, ui ) {
+// 	var id = event.target.id;
+// 	var lowerVal = ui.values[0]
+//     var upperVal  = ui.values[1]
+// 	currentSliderChange(id,lowerVal, upperVal); // call slider change function.
+// });
+// $('#humiditySldr').on('slide', function( event, ui ) {
+// 	console.log('humidity slider slide');
+// 	$('#humiditySldr').prev().find('span').each(function(i,v){
+//         $(v).html(ui.values[i])
+//     })
+// });
 // Pressure slider Handeler //////////////////////////////////////////////////////////////////////////////////////////
 // $('#pressureSldr').slider({range:true, min:0, max:100, values:[0,100]});
 // $('#pressureSldr').on('slidechange', function( event, ui ) {
